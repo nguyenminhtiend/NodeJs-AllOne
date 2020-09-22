933d969d2d5a3de3e9f1dddcf98bc4d15c7b87c5
Tien Nguyen <nguyenminhtiend@gmail.com>
2020-09-22 22:26:15 +0700

    test commit

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 const app = require('./app');
 
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 
 const server = app().listen(port, () => {
   console.log(`Server (env: ${process.env.NODE_ENV}) started on ${port}`);
